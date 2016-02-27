9e60bbcf01a233d55e776ef9e112b45a512b7fe7
Richard Min <richardmin97@gmail.com>
2016-02-27 15:43:07 -0800

    not sure

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,14 @@ var express = require('express');
 //var twit = require('twit');
 //loads the access tokens into the environment
 require('dotenv').load();
-
 var router = express.Router();
-//
-//var T = new twit({
-//  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
-//  consumer_secret: 'process.env.TWITTER_CONSUMER_SECRET',
-//  app_only_auth: true
-//});
+var azure = require('azure-storage');
 
-/*
- * Wrapper function for making calls to twitter.
- * Params should be a json object, with a mandatory q= field
- * Returns data to the callback function.
- */
-//twitQuery = function (params, callback)
-//{
-//  T.get('search/tweets', params, function(err, reply) {
-//    if(!err) return callback(reply);
-//
-//  });
-//}
+//Todo: Find AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_ACCESS_KEY, AZURE_STORAGE_CONNECTION_STRING
+//From Azure, place into the .env
+var tableSvc = azure.createTableService();
 
 
-function returnError() {
-
-}
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
