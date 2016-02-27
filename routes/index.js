b729bb3cd8745d5fc842c455d036adf30201b7a6
Richard Min <richardmin97@gmail.com>
2016-02-27 14:10:24 -0800

    changed npm module from twitter to twit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
 var express = require('express');
+//var Twitter = require('Twitter');
+require('dotenv').load();
+
 var router = express.Router();
+//
+//var client = new Twitter({
+//  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
+//  consumer_secret: 'process.env.TWITTER_CONSUMER_SECRET',
+//  bearer_token: 'process.env.TWITTER_BEARER_TOKEN'
+//});
+
+//
+//client.get('favorites/list', function(error, tweets, response){
+//  if(error) throw error;
+//  console.log(tweets);  // The favorites.
+//  console.log(response);  // Raw response object.
+//});
+//
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
