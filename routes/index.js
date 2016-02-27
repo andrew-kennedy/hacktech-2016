13eef7c0dbd10c23a11f42563bb83b4e13b2ccf3
Richard Min <richardmin97@gmail.com>
2016-02-27 15:10:10 -0800

    whoops

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,24 @@
 var express = require('express');
-//var Twitter = require('Twitter');
+var Twitter = require('Twitter');
+//loads the access tokens into the environment
 require('dotenv').load();
 
 var router = express.Router();
-//
-//var client = new Twitter({
-//  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
-//  consumer_secret: 'process.env.TWITTER_CONSUMER_SECRET',
-//  bearer_token: 'process.env.TWITTER_BEARER_TOKEN'
-//});
 
-//
-//client.get('favorites/list', function(error, tweets, response){
-//  if(error) throw error;
-//  console.log(tweets);  // The favorites.
-//  console.log(response);  // Raw response object.
-//});
-//
+var client = new Twitter({
+  consumer_key: 'process.env.TWITTER_CONSUMER_KEY',
+  consumer_secret: 'process.env.TWITTER_CONSUMER_SECRET',
+  bearer_token: 'process.env.TWITTER_BEARER_ACCESS_TOKEN'
+});
+
+
+client.get('search/tweets', {q: "bernie sanders"}, function(error, tweets, response){
+  console.log("error");
+  //if(error) throw error;
+  //console.log(tweets);  // The favorites.
+  console.log(response);  // Raw response object.
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
