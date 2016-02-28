3e8e894d254553cd9e4137727f81547de0f2f3e4
notanotherbrick <kishansachdeva@gmail.com>
2016-02-28 05:13:43 -0800

    Pushing topFrequency script

--- R Scripts/topFreq.R
+++ R Scripts/topFreq.R
@@ -19,3 +19,32 @@ library(cluster)
 library(fpc)
 library(igraph)
 
+# Create Corpus
+
+docs <- Corpus(VectorSource(tweet_x$status))
+# summary(docs)
+
+#remove punctuations
+docs <- tm_map(docs, removePunctuation)
+#remove spl characters, numbers
+for(j in seq(docs))   
+{   
+  docs[[j]] <- gsub("/", " ", docs[[j]])   
+  docs[[j]] <- gsub("@", " ", docs[[j]])   
+  docs[[j]] <- gsub("\\|", " ", docs[[j]])   
+}   
+
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, tolower) 
+docs <- tm_map(docs, removeWords, stopwords("english"))
+
+# Combining words that should stay together
+for (j in seq(docs))
+{
+  docs[[j]] <- gsub("qualitative research", "QDA", docs[[j]])
+  docs[[j]] <- gsub("qualitative studies", "QDA", docs[[j]])
+  docs[[j]] <- gsub("qualitative analysis", "QDA", docs[[j]])
+  docs[[j]] <- gsub("research methods", "research_methods", docs[[j]])
+}
+
+
