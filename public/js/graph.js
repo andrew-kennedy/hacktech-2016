58ca322d7827d062981773c54c6733fb91863292
Andrew Kennedy <andrewk36@gmail.com>
2016-02-28 04:50:33 -0800

    added graph zooming

--- public/js/graph.js
+++ public/js/graph.js
@@ -6,16 +6,16 @@ var insertGraph = function () {
             data[i] = MG.convert.date(data[i], 'date');
         }
 
-        data[1][data[1].length-1].value = 50000000;
-        data[2][data[2].length-1] = MG.clone(data[1][data[1].length-1]);
-        data[2][data[2].length-1].value += 10000000;
-
+        console.log(data);
         MG.data_graphic({
             title: "Labeling Lines",
             data: data,
             width: 600,
             height: 200,
             animate_on_load: true,
+            missing_is_hidden: true,
+            brushing: true,
+            brushing_history: true,
             transition_on_update: true,
             legend: ['US', 'CA', 'DE'],
             target: '.graph-container'
