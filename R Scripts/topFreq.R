d7c24e525fa756f8ee79e53744744387c38cbb93
notanotherbrick <kishansachdeva@gmail.com>
2016-02-28 05:13:06 -0800

    Pushing script for high frq words

---
+++ R Scripts/topFreq.R
@@ -0,0 +1,21 @@
+# Script for calculating High Frequency words
+
+#########################
+## check onces! - if all libraries are present
+
+# Needed <- c("tm", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc")   
+# install.packages(Needed, dependencies=TRUE)   
+
+# install.packages("Rcampdf", repos = "http://datacube.wu.ac.at/", type = "source")
+#########################
+
+library(tm)
+library(SnowballCC)
+library(RColorBrewer)
+library(ggplot2)
+library(wordcloud)
+library(biclust)
+library(cluster)
+library(fpc)
+library(igraph)
+
